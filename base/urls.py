92bc2c73a16883d3a107e0704c80daa57baf2786
Devanshvora03 <131452370+Devanshvora03@users.noreply.github.com>
2023-08-27 09:22:58 +0530

    prefinal

--- base/urls.py
+++ base/urls.py
@@ -3,16 +3,17 @@ from . import views
 
 urlpatterns = [
     path('lobby/', views.lobby),
-    path('studentd/', views.studentd, name='studentd'),
-
     path('room/', views.room),
+
     path('get_token/', views.getToken),
     path('create_member/', views.createMember),
     path('get_member/', views.getMember),
     path('delete_member/', views.deleteMember),
-    # path('register/', views.register, name="register"),
-    # path('index/', views.index, name="index"),
-    path('', views.register, name="register"),
-    path('login',views.login),
 
+    path('studentd/', views.studentd, name='studentd'),
+    path('facultyd/', views.studentd, name='facultyd'),
+
+    path('', views.register, name="register"),
+    path('login/',views.login),
+    path('schedule/',views.schedule),
 ]
--- base/views.py
+++ base/views.py
@@ -9,76 +9,29 @@ from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth import authenticate, login, get_user_model
 
 
-
-
-# Create your views here.
-
 def lobby(request):
     return render(request, 'base/lobby.html')
-
-# def index(request):
-#     return render(request, 'base/index.html')
 
 def room(request):
     return render(request, 'base/room.html')
 
-
-
 def register(request):
-    print('in register')
-    print(request)
-    if request.method == "POST":
-        print('inpost')
-        email = request.POST.get('email')
-        password = request.POST.get('password')
-        username = request.POST.get('uname')
-        
-        print(username)
-        # user = authenticate(email = email,password =password)
-        user = User.objects.create_user(username, email, password)
-        message ='signup successfully'
-    else:
-        print('not post')
-        return render(request, 'base/register.html')
+    return render(request, 'base/register.html')
 
-        
-        
-    
 def main(request):
-    return render(request,'main.html')
+    return render(request,'base/main.html')
 
 def studentd(request):
-    return render(request, 'studentd.html')
+    return render(request, 'base/studentd.html')
 
+def facultyd(request):
+    return render(request, 'base/facultyd.html')
 
 def login(request):
-    
-    if request.user.is_authenticated:
-        return redirect('main')
-       
-    if request.method =="POST":
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        # username = User.objects.get(email=email).username
-        # user_id = User.objects.get(email=email).id
-        # data = {
-        #     'username':username,
-        #     'eamil':email,
-        #     'user_id':user_id,
-        # }
-        # print(data)
-        user = authenticate(username=username, password=password)
-        if user is not None:
-            login(request, user)
-        return redirect('main')
-    else:
-        return redirect('login.html')
-              
     return render(request, 'base/login.html')
 
-    
-    
-    return render(request, 'base/login.html')
+def schedule(request):
+    return render(request, 'base/schedule.html')
 
 def getToken(request):
     appId = "6bb17f29e6484385bd9cd18f62d29512"
